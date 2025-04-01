06d8e7993796d005e6f5c6a822705701da8dbaed
wenerd569 <wenerd569@gmail.com>
2025-04-01 11:33:51 +0300

    Itit backend

---
+++ backend/superapp/src/main/java/wenerd/other/superapp/SuperappApplication.java
@@ -0,0 +1,13 @@
+package wenerd.other.superapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperappApplication.class, args);
+	}
+
+}
